23dcb8093033e862669948e542a82445e97f4da3
name <email@com>
2023-11-24 21:22:08 +0300

    добавлены комментарии

--- ParseCitySubjson.cpp
+++ ParseCitySubjson.cpp
@@ -1,23 +1,25 @@
 // обрабатывает JSON-объект со списком городов конкретной страны
+// модифицирует полученный вектор cities_dst
 // вызывается только из функции ParseCountryJson
 void ParseCitySubjson(vector<City>& cities_dst, const Json& cities_json, const Country& country)
 {
     for (const auto& city_json : cities_json.AsList()) {
         const auto& city_obj = city_json.AsObject();
         cities_dst.push_back({
-            .name             = city_obj["name"s].AsString(),
-            .iso_code         = city_obj["iso_code"s].AsString(),
-            .phone_code       = country.phone_code + city_obj["phone_code"s].AsString(),
-            .country_name     = country.name,
-            .country_iso_code = country.iso_code,
-            .time_zone        = country.time_zone,
-            .languages        = country.languages
-        });
+                                     .name             = city_obj["name"s].AsString(),
+                                     .iso_code         = city_obj["iso_code"s].AsString(),
+                                     .phone_code       = country.phone_code + city_obj["phone_code"s].AsString(),
+                                     .country_name     = country.name,
+                                     .country_iso_code = country.iso_code,
+                                     .time_zone        = country.time_zone,
+                                     .languages        = country.languages
+                             });
     }
 }
 
 
 // обрабатывает JSON-объект со списком языков конкретной страны
+// модифицирует полученный вектор languages_dst
 // вызывается только из функции ParseCountryJson
 void ParseLanguagesList(vector<Language>& languages_dst, const Json& languages_json)
 {
@@ -32,11 +34,11 @@ void ParseCountryJson(vector<Country>& countries, vector<City>& cities, const Js
     for (const auto& country_json : json.AsList()) {
         const auto& country_obj = country_json.AsObject();
         countries.push_back({
-            .name       = country_obj["name"s].AsString(),
-            .iso_code   = country_obj["iso_code"s].AsString(),
-            .phone_code = country_obj["phone_code"s].AsString(),
-            .time_zone  = country_obj["time_zone"s].AsString(),
-        });
+                                    .name       = country_obj["name"s].AsString(),
+                                    .iso_code   = country_obj["iso_code"s].AsString(),
+                                    .phone_code = country_obj["phone_code"s].AsString(),
+                                    .time_zone  = country_obj["time_zone"s].AsString(),
+                            });
         Country& country = countries.back();
         ParseLanguagesList(country.languages, country_obj["languages"s]);
         ParseCitySubjson(cities, country_obj["cities"s], country);
