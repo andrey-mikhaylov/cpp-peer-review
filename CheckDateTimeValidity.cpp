a9d0476065d528435d7cccf00480e9915624bd7a
en-ot <andrey.mikhaylov.n@gmail.com>
2023-11-24 09:53:02 +0300

    CheckDateTimeValidity refactored

--- CheckDateTimeValidity.cpp
+++ CheckDateTimeValidity.cpp
@@ -1,46 +1,21 @@
-void CheckDateTimeValidity(const DateTime& dt) {
-    if (dt.year < 1) {
-        throw domain_error("year is too small"s);
-    }
-    if (dt.year > 9999) {
-        throw domain_error("year is too big"s);
-    }
+void CheckDateTimeValidity(const DateTime& dt)
+{
+    auto ThrowIfInvalid = [](int value, const string& name, int min, int max) {
+        if (value < min)
+            throw domain_error(name + "is too small"s);
 
-    if (dt.month < 1) {
-        throw domain_error("month is too small"s);
-    }
-    if (dt.month > 12) {
-        throw domain_error("month is too big"s);
-    }
+        if (value > max)
+            throw domain_error(name + "is too big"s);
+    };
+
+    ThrowIfInvalid(dt.year,  "year"s,  1, 9999);
+    ThrowIfInvalid(dt.month, "month"s, 1, 12);
 
     const bool is_leap_year = (dt.year % 4 == 0) && !(dt.year % 100 == 0 && dt.year % 400 != 0);
     const array month_lengths = {31, 28 + is_leap_year, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    ThrowIfInvalid(dt.day, "day"s, 1, month_lengths[dt.month-1]);
 
-    if (dt.day < 1) {
-        throw domain_error("day is too small"s);
-    }
-    if (dt.day > month_lengths[dt.month - 1]) {
-        throw domain_error("day is too big"s);
-    }
-
-    if (dt.hour < 0) {
-        throw domain_error("hour is too small"s);
-    }
-    if (dt.hour > 23) {
-        throw domain_error("hour is too big"s);
-    }
-
-    if (dt.minute < 0) {
-        throw domain_error("minute is too small"s);
-    }
-    if (dt.minute > 59) {
-        throw domain_error("minute is too big"s);
-    }
-
-    if (dt.second < 0) {
-        throw domain_error("second is too small");
-    }
-    if (dt.second > 59) {
-        throw domain_error("second is too big"s);
-    }
+    ThrowIfInvalid(dt.hour,   "hour"s,   0, 23);
+    ThrowIfInvalid(dt.minute, "minute"s, 0, 59);
+    ThrowIfInvalid(dt.second, "second"s, 0, 59);
 }
