c13293917ed96d1a3034deaefea524b273f29fc9
name <email@com>
2023-11-24 21:09:18 +0300

    Вынесены функции проверки года на високосность и выдачи числа дней в месяце

--- CheckDateTimeValidity.cpp
+++ CheckDateTimeValidity.cpp
@@ -1,3 +1,16 @@
+bool IsLeapYear(int year)
+{
+    return (year % 4 == 0) && !(year % 100 == 0 && year % 400 != 0);
+}
+
+
+int DaysInMonth(int year, int month)
+{
+    const array month_lengths = {31, IsLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    return month_lengths[month-1];
+}
+
+
 void CheckDateTimeValidity(const DateTime& dt)
 {
     auto ThrowIfInvalid = [](int value, const string& name, int min, int max) {
@@ -10,11 +23,7 @@ void CheckDateTimeValidity(const DateTime& dt)
 
     ThrowIfInvalid(dt.year,  "year"s,  1, 9999);
     ThrowIfInvalid(dt.month, "month"s, 1, 12);
-
-    const bool is_leap_year = (dt.year % 4 == 0) && !(dt.year % 100 == 0 && dt.year % 400 != 0);
-    const array month_lengths = {31, 28 + is_leap_year, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    ThrowIfInvalid(dt.day, "day"s, 1, month_lengths[dt.month-1]);
-
+    ThrowIfInvalid(dt.day, "day"s, 1, DaysInMonth(dt.year, dt.month));
     ThrowIfInvalid(dt.hour,   "hour"s,   0, 23);
     ThrowIfInvalid(dt.minute, "minute"s, 0, 59);
     ThrowIfInvalid(dt.second, "second"s, 0, 59);
